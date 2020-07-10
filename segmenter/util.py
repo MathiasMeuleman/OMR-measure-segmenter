75bbcf1a47ebcedcae8f4b5f70683ac3ce09757e
MathiasMeuleman <meuleman.mathias@gmail.com>
2020-07-10 12:17:34 +0200

    Swap preprocessor for Hough lines based rotation

--- segmenter/util.py
+++ segmenter/util.py
@@ -1,5 +1,12 @@
+import math
+
+from PIL import Image
+from scipy import ndimage
+
 import cv2
 import numpy as np
+
+from segmenter.image_util import resize_img
 
 
 def consecutive(data, stepsize=1):
@@ -27,7 +34,7 @@ def compare_measure_bounding_boxes(self, other):
                 return -1
 
 
-def preprocess(_img):
+def preprocess_minrect(_img):
     """
     Takes a numpy array of (w x h x 3) representing the image.
     Binarizes the image (color 2 grayscale) and rotates the image such that its smallest rectangular bounding box
@@ -54,3 +61,26 @@ def preprocess(_img):
     rotated = cv2.warpAffine(_img, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
     return rotated
 
+
+def preprocess(img):
+    """
+    Preprocess the image in a few steps:
+      - Use Otsu thresholding to binarize the image
+      - Invert the image, for easier processing
+      - Rotate the image to a correct alignment. The median angle for detected Hough lines is taken as the rotation angle
+    :param img: The original image, as NumPy array
+    :return: The binarized, inverted, rotated image, as NumPy image
+    """
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img_bw = cv2.threshold(img_gray, 0, 255, cv2.THRESH_OTSU)[1]
+    img_bw = cv2.bitwise_not(img_bw)
+    img_edges = cv2.Canny(img_gray, 100, 100, apertureSize=3)
+    lines = cv2.HoughLinesP(img_edges, 1, math.pi / 180.0, 100, minLineLength=100, maxLineGap=5)
+    angles = []
+    for line in lines:
+        x1, y1, x2, y2 = line[0]
+        angles.append(math.degrees(math.atan2(y2 - y1, x2 - x1)))
+        cv2.line(img_gray, (x1, y1), (x2, y2), (255, 0, 0), 3)
+    median_angle = np.median(angles)
+    rotated = ndimage.rotate(img_bw, median_angle)
+    return rotated
