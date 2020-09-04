06bfd106665a694960b087772d3419f7fb597cd8
MathiasMeuleman <meuleman.mathias@gmail.com>
2020-09-04 15:19:28 +0200

    Improve pdf2png util

--- util/pdf2png.py
+++ util/pdf2png.py
@@ -6,9 +6,10 @@ from pdf2image.exceptions import (
     PDFSyntaxError
 )
 from PyPDF2 import PdfFileReader
+from pathlib import Path
 
 
-def convert_pdf_file(_path, first_page=1, last_page=-1):
+def convert_pdf_file(_path, out_path, first_page=1, last_page=-1):
     pdf = PdfFileReader(open(_path, 'rb'))
     max_pages = pdf.numPages
     if last_page > 0:
@@ -18,10 +19,10 @@ def convert_pdf_file(_path, first_page=1, last_page=-1):
         with tempfile.TemporaryDirectory() as outpath:
             images = pdf2image.convert_from_path(_path, dpi=600, output_folder=outpath, first_page=page, last_page=min(page + 10 - 1, max_pages))
             for image in images:
-                image.save("transcript-{0}.png".format(index))
+                image.save(str((Path(out_path) / "transcript-{0}.png".format(index)).resolve()))
                 index += 1
 
 
 if __name__ == "__main__":
-    path = r'C:\Users\Mathias\Documents\TU Delft\Thesis\thesis\data\IMSLP17070-Mahler-Symph1fs.pdf'
-    convert_pdf_file(path)
+    path = r"../data/Beethoven_Andante_FMajor/IMSLP109835-PMLP30951-sibley.1802.1525.beethoven.andante.fmajor.pdf"
+    convert_pdf_file(path, out_path=r"../data/Beethoven_Andante_FMajor/ppm-600")
