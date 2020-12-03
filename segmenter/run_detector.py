2772967feb2d6f2acb66f4ff3a5d41e06c1d463e
MathiasMeuleman <meuleman.mathias@gmail.com>
2020-12-03 16:52:26 +0100

    Improve evaluation

--- segmenter/run_detector.py
+++ segmenter/run_detector.py
@@ -6,7 +6,6 @@ from collections import namedtuple
 from segmenter.old.image_util import data_dir, tmp_dir
 from segmenter.measure_detector import detect_measures
 
-score = 'Beethoven_Sextet'
 TrueSystem = namedtuple('TrueSystem', ['staffs', 'measures'])
 TruePage = namedtuple('TruePage', ['systems'])
 
@@ -19,37 +18,34 @@ def get_sorted_page_paths(page_path):
 
 def compare_results(detected_pages, true_pages):
     outputs = []
-    correct_system_count = 0
-    correct_measure_count = 0
-    correct_staff_count = 0
+    detected_system_count = 0
+    detected_measure_count = 0
+    detected_staff_count = 0
     total_systems = sum([len(p.systems) for p in true_pages])
     total_measures = sum([sum([s.measures for s in p.systems]) for p in true_pages])
     total_staffs = sum([sum([s.staffs for s in p.systems]) for p in true_pages])
     for i in range(len(detected_pages)):
-        if len(detected_pages[i].systems) == len(true_pages[i].systems):
-            correct_system_count += len(true_pages[i].systems)
-        else:
+        detected_system_count += len(detected_pages[i].systems)
+        if len(detected_pages[i].systems) != len(true_pages[i].systems):
             outputs.append('Page {}: Expected {} systems, but got {}'.format(i, len(true_pages[i].systems), len(detected_pages[i].systems)))
-    if correct_system_count == total_systems:
+    if detected_system_count == total_systems:
         for i in range(len(detected_pages)):
             detected_systems, true_systems = detected_pages[i].systems, true_pages[i].systems
             for j in range(len(detected_pages[i].systems)):
-                if len(detected_systems[j].measures) == true_systems[j].measures:
-                    correct_measure_count += true_systems[j].measures
-                else:
+                detected_measure_count += len(detected_systems[j].measures)
+                if len(detected_systems[j].measures) != true_systems[j].measures:
                     outputs.append('Page {}, system {}: Expected {} measures, but got {}'.format(i, j, true_systems[j].measures, len(detected_systems[j].measures)))
                 system_staffs = list(map(lambda k: len(detected_systems[j].measures[k].staffs), range(len(detected_systems[j].measures))))
                 if len(set(system_staffs)) > 1:
                     outputs.append('Page {}, system {}: Not all measures have equal numbers of staffs'.format(i, j))
                 else:
-                    if system_staffs[0] == true_systems[j].staffs:
-                        correct_staff_count += true_systems[j].staffs
-                    else:
+                    detected_staff_count += system_staffs[0]
+                    if system_staffs[0] != true_systems[j].staffs:
                         outputs.append('Page {}, system {}: Expected {} staffs, but got {}'.format(i, j, true_systems[j].staffs, len(detected_systems[j].measures[0].staffs)))
     print('Test results:\n=============')
-    print('System score: {}/{} ({}%)'.format(correct_system_count, total_systems, round(correct_system_count/total_systems * 100)))
-    print('Measure score: {}/{} ({}%)'.format(correct_measure_count, total_measures, round(correct_measure_count/total_measures * 100)))
-    print('Staff score: {}/{} ({}%)'.format(correct_staff_count, total_staffs, round(correct_staff_count/total_staffs * 100)))
+    print('System score: {}/{} ({}%)'.format(detected_system_count, total_systems, round(detected_system_count/total_systems * 100)))
+    print('Measure score: {}/{} ({}%)'.format(detected_measure_count, total_measures, round(detected_measure_count/total_measures * 100)))
+    print('Staff score: {}/{} ({}%)'.format(detected_staff_count, total_staffs, round(detected_staff_count/total_staffs * 100)))
     print('\n'.join(outputs))
 
 
@@ -63,7 +59,7 @@ def evaluate(score_name):
         baseline = [list(map(build_true_system, line.rstrip().split(' '))) for line in file]
     true_pages = list(map(lambda s: TruePage(systems=s), baseline))
 
-    page_paths = get_sorted_page_paths(join(data_dir, score_name, 'ppm-600'))
+    page_paths = get_sorted_page_paths(join(data_dir, score_name, 'ppm-300'))
     if len(page_paths) != len(true_pages):
         print('Expected {} pages, but got {}'.format(len(true_pages), len(page_paths)))
         return
@@ -97,8 +93,8 @@ def draw_page(img, page):
     img.show()
 
 
-def save_pages(images, pages):
-    pdf_filename = Path(data_dir, score, 'segmented_visual.pdf').resolve()
+def save_pages(images, pages, score_name):
+    pdf_filename = Path(data_dir, score_name, 'segmented_visual.pdf').resolve()
     im1 = construct_page_overlay(images[0], pages[0])
     im_list = []
     for i in range(1, len(images)):
@@ -107,20 +103,23 @@ def save_pages(images, pages):
     im1.save(pdf_filename, 'PDF', resolution=100.0, save_all=True, append_images=im_list)
 
 
-def detect():
-    # page_path = Path(data_dir, score, 'ppm-600').resolve()
-    page_path = Path(tmp_dir, 'single').resolve()
+def detect(score_name):
+    page_path = Path(data_dir, score_name, 'ppm-300').resolve()
+    # page_path = Path(tmp_dir, 'single').resolve()
     paths = get_sorted_page_paths(page_path)
     originals = []
     pages = []
     for i, path in tqdm(enumerate(paths)):
         original = Image.open(path)
-        page = detect_measures(path)
-        draw_page(original, page)
-        originals.append(original)
+        page = detect_measures(path, plot=False)
+        # draw_page(original, page)
+        originals.append(original.rotate(page.rotation, fillcolor='white'))
         pages.append(page)
-    # save_pages(originals, pages)
+    save_pages(originals, pages, score_name)
 
 
 if __name__ == '__main__':
-    evaluate(score)
+    scores = ['Van_Bree_Allegro']
+    for score in scores:
+        # evaluate(score)
+        detect(score)
