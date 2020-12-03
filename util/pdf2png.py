7b35c05e856936bd8d69ce0d250ac3644cba8885
MathiasMeuleman <meuleman.mathias@gmail.com>
2020-12-03 16:44:17 +0100

    Update utils

--- util/pdf2png.py
+++ util/pdf2png.py
@@ -7,22 +7,24 @@ from pdf2image.exceptions import (
 )
 from PyPDF2 import PdfFileReader
 from pathlib import Path
+from tqdm import tqdm
 
 
 def convert_pdf_file(_path, out_path, first_page=1, last_page=-1):
+    Path(out_path).mkdir(exist_ok=True)
     pdf = PdfFileReader(open(_path, 'rb'))
     max_pages = pdf.numPages
     if last_page > 0:
         max_pages = min(max_pages, last_page)
     index = first_page
-    for page in range(first_page, max_pages, 10):
+    for page in tqdm(range(first_page, max_pages, 10)):
         with tempfile.TemporaryDirectory() as outpath:
-            images = pdf2image.convert_from_path(_path, dpi=600, output_folder=outpath, first_page=page, last_page=min(page + 10 - 1, max_pages))
+            images = pdf2image.convert_from_path(_path, dpi=300, output_folder=outpath, first_page=page, last_page=min(page + 10 - 1, max_pages))
             for image in images:
                 image.save(str((Path(out_path) / "transcript-{0}.png".format(index)).resolve()))
                 index += 1
 
 
 if __name__ == "__main__":
-    path = r"../data/Beethoven_Sextet/IMSLP34708-SIBLEY1802.7190.19715.2169-39087018981599score.pdf"
-    convert_pdf_file(path, out_path=r"../data/Beethoven_Sextet/ppm-600")
+    path = r"../data/Van_Bree_Allegro/IMSLP82485-PMLP136878-1602.Bree-Allegro4SQs.sc.pdf"
+    convert_pdf_file(path, out_path=r"../data/Van_Bree_Allegro/ppm-300")
---
+++ util/split_score.py
@@ -0,0 +1,33 @@
+from PIL import Image
+from pathlib import Path
+from segmenter.old.image_util import data_dir
+from util.PIL_util import resize_img
+
+
+def split_img_vertical(img_path):
+    img = Image.open(img_path)
+    w,h = img.size
+    left = img.crop((0, 0, w//2, h))
+    right = img.crop((w//2, 0, w, h))
+    return [left, right]
+
+
+def get_sorted_page_paths(page_path):
+    paths = [p for p in Path(page_path).iterdir() if p.is_file()]
+    return [str(p.resolve()) for p in sorted(paths, key=lambda p: int(str(p.stem).split("-")[1]))]
+
+
+def split_pages(folder, out_path):
+    Path(out_path).mkdir(exist_ok=True)
+    paths = get_sorted_page_paths(folder)
+    for i, path in enumerate(paths):
+        imgs = split_img_vertical(path)
+        imgs[0].save(Path(out_path, 'transcript-{}.png'.format((i+1)*2 - 1)))
+        imgs[1].save(Path(out_path, 'transcript-{}.png'.format((i+1)*2)))
+
+
+if __name__ == '__main__':
+    score = 'Van_Bree_Allegro'
+    folder = Path(data_dir, score, 'ppm-300-pre')
+    out_path = Path(data_dir, score, 'ppm-300')
+    split_pages(folder, out_path)
