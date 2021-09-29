a79fcca85042cd443e645245d9d08d942ffcd171
MathiasMeuleman <meuleman.mathias@gmail.com>
2021-09-29 10:33:39 +0200

    Refactor StaffDetector into single image use

--- score_analysis/staff_detector.py
+++ score_analysis/staff_detector.py
@@ -1,69 +1,46 @@
 import json
-import subprocess
-from pathlib import Path
 
-from PIL import Image
+from PIL import Image, ImageColor, ImageDraw
 from tqdm import tqdm
 
+from score_analysis.score_image import ScoreImage
 from score_analysis.stafffinder_meuleman import StaffFinder_meuleman
-from util.dirs import musicdata_dir
-from util.pdf2png import convert_pdf_file
+from util.dirs import data_dir
 
 
 class StaffDetector:
 
-    def __init__(self, directory, staff_finder):
-        self.directory = Path(directory)
-        self.staff_finder = staff_finder
+    def __init__(self, image, output_path=None):
+        self.image = image if isinstance(image, ScoreImage) else ScoreImage(image)
+        self.output_path = output_path
 
-    def get_score_path(self):
-        score_path = next((f for f in self.directory.iterdir() if f.suffix == '.pdf'), None)
-        if not score_path.is_file():
-            raise FileNotFoundError('Could not find PDF score file in directory ' + str(self.directory))
-        return score_path
-
-    def verify_pages_exist(self):
-        pages_path = self.directory / 'pages'
-        if not pages_path.is_dir():
-            pages_path.mkdir()
-        if not (pages_path / 'page_1.png').is_file():
-            score_path = self.get_score_path()
-            convert_pdf_file(score_path, pages_path)
-
-    def run_py2_detect_staffs(self, image_path, output_path):
-        detect_staffs_path = (Path(__file__).parent / 'py2_detect_staffs.py').absolute()
-        subprocess.run(['/usr/bin/python', detect_staffs_path, image_path.absolute(), output_path.absolute(), self.staff_finder])
-
-    def detect_page_staffs(self, image_path):
-        image = Image.open(image_path)
-        staff_finder = StaffFinder_meuleman(image)
+    def detect_staffs(self):
+        staff_finder = StaffFinder_meuleman(self.image)
         staff_finder.find_staves(debug=0)
         staves = staff_finder.get_average()
-        staff_points = [{'lines': [[[line.left_x, line.average_y], [line.right_x, line.average_y]] for line in staff]} for staff in staves]
-        return {'staves': staff_points}
-
-    def detect_staffs(self, legacy=False):
-        self.verify_pages_exist()
-        staffs_path = self.directory / 'staffs' / self.staff_finder
-        pages_path = self.directory / 'pages'
-        if not staffs_path.is_dir():
-            staffs_path.mkdir(parents=True)
-        staffs = []
-        for i in tqdm(range(len(list(pages_path.iterdir())))):
-            image_path = pages_path / 'page_{}.png'.format(i+1)
-            output_path = staffs_path / 'page_{}.json'.format(i+1)
-            if legacy:
-                self.run_py2_detect_staffs(image_path, output_path)
-                with open(output_path) as f:
-                    staffs.append(json.load(f))
-            else:
-                page_staffs = self.detect_page_staffs(image_path)
-                staffs.append(page_staffs)
-                with open(output_path, 'w') as f:
-                    f.write(json.dumps(page_staffs, sort_keys=True, indent=2))
+        staffs = {'staves': [
+            {'lines': [[[line.left_x, line.average_y], [line.right_x, line.average_y]] for line in staff]}
+            for staff in staves]}
+        if self.output_path is not None:
+            with open(self.output_path, 'w') as f:
+                f.write(json.dumps(staffs, sort_keys=True, indent=2))
         return staffs
 
 
 if __name__ == '__main__':
-    score_dir = musicdata_dir / 'bach_brandenburg_concerto_5_part_1'
-    StaffDetector(score_dir, 'Meuleman').detect_staffs()
+    staff_path = data_dir / 'sample' / 'staffs'
+    staff_path.mkdir(parents=True, exist_ok=True)
+    overlay_path = data_dir / 'sample' / 'staff_overlays' / 'pages'
+    overlay_path.mkdir(parents=True, exist_ok=True)
+    for image_path in tqdm((data_dir / 'sample' / 'pages').iterdir()):
+        image = Image.open(image_path)
+        staffs = StaffDetector(image, output_path=staff_path / (image_path.stem + '.json')).detect_staffs()
+        staff_image = image.convert('RGB')
+        colors = ['red', 'orange', 'green', 'blue', 'purple']
+        for i, staff in enumerate(staffs['staves']):
+            color = ImageColor.getrgb(colors[i % len(colors)])
+            for line in staff['lines']:
+                draw = ImageDraw.Draw(staff_image, mode='RGBA')
+                draw.line([tuple(point) for point in line], fill=color, width=5)
+                del draw
+        staff_image.save(overlay_path / image_path.name)
---
+++ score_analysis/staff_detector_legacy.py
@@ -0,0 +1,47 @@
+import json
+import subprocess
+from pathlib import Path
+
+from PIL import Image, ImageColor, ImageDraw
+from tqdm import tqdm
+
+from util.dirs import data_dir
+
+
+class StaffDetectorLegacy:
+
+    def __init__(self, image_path, staff_finder, output_path=None):
+        self.image_path = Path(image_path)
+        self.staff_finder = staff_finder
+        self.output_path = output_path
+
+    def run_py2_detect_staffs(self, image_path, output_path):
+        detect_staffs_path = (Path(__file__).parent / 'py2_detect_staffs.py').absolute()
+        subprocess.run(['/usr/bin/python', detect_staffs_path, image_path.absolute(), output_path.absolute(), self.staff_finder])
+
+    def detect_staffs(self):
+        output_path = self.image_path.parents[0] / '.tmp-{}.json'.format(self.image_path.stem) if self.output_path is None else self.output_path
+        self.run_py2_detect_staffs(self.image_path, output_path)
+        with open(output_path) as f:
+            staffs = json.load(f)
+        return staffs
+
+
+if __name__ == '__main__':
+    staff_path = data_dir / 'sample' / 'legacy' / 'staffs'
+    staff_path.mkdir(parents=True, exist_ok=True)
+    overlay_path = data_dir / 'sample' / 'legacy' / 'staff_overlays' / 'pages'
+    overlay_path.mkdir(parents=True, exist_ok=True)
+    for image_path in tqdm((data_dir / 'sample' / 'pages').iterdir()):
+        image = Image.open(image_path)
+        staffs = StaffDetectorLegacy(image_path, staff_finder='Dalitz',
+                                     output_path=staff_path / (image_path.stem + '.json')).detect_staffs()
+        staff_image = image.convert('RGB')
+        colors = ['red', 'orange', 'green', 'blue', 'purple']
+        for i, staff in enumerate(staffs['staves']):
+            color = ImageColor.getrgb(colors[i % len(colors)])
+            for line in staff['lines']:
+                draw = ImageDraw.Draw(staff_image, mode='RGBA')
+                draw.line([tuple(point) for point in line], fill=color, width=5)
+                del draw
+        staff_image.save(overlay_path / image_path.name)
